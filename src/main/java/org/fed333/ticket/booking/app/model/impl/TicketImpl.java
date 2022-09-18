8fdea8881c87262ff5fcb5b09fd351ebaf12f252
Roman_Kovalchuk <Roman_Kovalchuk@epam.com>
2022-09-19 00:57:16 +0300

    Linked Event and User in Ticket.

--- src/main/java/org/fed333/ticket/booking/app/model/impl/TicketImpl.java
+++ src/main/java/org/fed333/ticket/booking/app/model/impl/TicketImpl.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.fed333.ticket.booking.app.model.Event;
 import org.fed333.ticket.booking.app.model.Ticket;
+import org.fed333.ticket.booking.app.model.User;
+
+import javax.persistence.OneToOne;
 
 @Data
 @Builder
@@ -16,7 +20,13 @@ public class TicketImpl implements Ticket {
 
     private Long eventId;
 
+    @OneToOne(targetEntity = EventImpl.class)
+    private Event event;
+
     private Long userId;
+
+    @OneToOne(targetEntity = UserImpl.class)
+    private User user;
 
     private Category category;
 
