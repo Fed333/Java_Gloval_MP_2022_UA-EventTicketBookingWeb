7d435b1c9b1c78dec643b2e3fa417f0df91fa06a
Roman_Kovalchuk <Roman_Kovalchuk@epam.com>
2022-09-26 16:27:38 +0300

    Added flyway migration to the project.

--- src/main/java/org/fed333/ticket/booking/app/config/HibernateConfig.java
+++ src/main/java/org/fed333/ticket/booking/app/config/HibernateConfig.java
@@ -57,9 +57,7 @@ public class HibernateConfig {
 
     private Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create");
-//        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-//        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "validate");
         hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 
         return hibernateProperties;
