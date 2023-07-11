8026e7f2ea0aa5f4e52ddb96275e48ffc894a321
Roman_Kovalchuk <Roman_Kovalchuk@epam.com>
2023-07-11 20:57:54 +0300

    Made unit test methods clear for further implementing

--- src/test/java/org/fed333/ticket/booking/app/service/EventServiceTest.java
+++ src/test/java/org/fed333/ticket/booking/app/service/EventServiceTest.java
@@ -1,162 +1,57 @@
 package org.fed333.ticket.booking.app.service;
 
-import lombok.extern.slf4j.Slf4j;
-import org.fed333.ticket.booking.app.exception.AlreadyExistsValidationException;
-import org.fed333.ticket.booking.app.exception.MissingIdValidationException;
 import org.fed333.ticket.booking.app.exception.event.EventAlreadyExistsValidationException;
 import org.fed333.ticket.booking.app.exception.event.EventMissingIdValidationException;
-import org.fed333.ticket.booking.app.model.entity.Event;
-import org.fed333.ticket.booking.app.repository.EventRepository;
-import org.fed333.ticket.booking.app.service.component.SaveEntityValidator;
-import org.fed333.ticket.booking.app.util.comparator.EventEqualityComparator;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.data.domain.Pageable;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.fed333.ticket.booking.app.utils.DateUtils.parseDate;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-@Slf4j
-@RunWith(MockitoJUnitRunner.class)
 public class EventServiceTest {
-
-    @Mock
-    private EventRepository mockedRepository;
-
-    @Mock
-    private SaveEntityValidator<Event,Long> mockedValidator;
-
-    @InjectMocks
-    private EventService eventService;
-
-    private Event testEvent;
-
-    private Date testDate;
-
-    private EventEqualityComparator eventEqualityComparator;
 
     @Before
     public void setUp() {
-        eventService.setSaveEventValidator(mockedValidator);
-        testEvent = Event.builder()
-                .id(1L)
-                .title("test event")
-                .date(new Date()).build();
-        eventEqualityComparator = new EventEqualityComparator();
-        testDate = parseDate("10-09-2022");
     }
 
     @Test
     public void getEventById_shouldReturnEvent() {
-        Long id = testEvent.getId();
-        when(mockedRepository.findById(id)).thenReturn(Optional.of(testEvent));
-
-        assertThat(eventService.getEventById(id)).isEqualTo(testEvent);
     }
 
     @Test
     public void getEventsByTitle_shouldReturnSubList() {
-        String title = testEvent.getTitle();
-        int cursor = 2, size = 4;
-        Pageable page = Pageable.ofSize(size).withPage(cursor);
-        List<Event> expectedList = Stream.iterate(5L, s -> s + 1).limit(4).map(this::createTestEvent).collect(Collectors.toList());
-        when(mockedRepository.findAllByTitle(title, page)).thenReturn(expectedList);
-
-        assertThat(new ArrayList<>(eventService.findAllByTitle(title, page))).usingComparatorForType(eventEqualityComparator, Event.class).isEqualTo(expectedList);
     }
 
     @Test
     public void getEventsForDay_shouldReturnSubList() {
-        Date date = parseDate("17-09-2022");
-        int cursor = 2, size = 4;
-        Pageable page = Pageable.ofSize(size).withPage(cursor);
-        List<Event> expectedList = Stream.iterate(5L, s -> s + 1).limit(4).map(this::createTestEvent).collect(Collectors.toList());
-        when(mockedRepository.findAllByDay(date, page)).thenReturn(expectedList);
-
-        assertThat(eventService.getEventsForDay(date, page)).usingElementComparator(eventEqualityComparator).isEqualTo(expectedList);
     }
 
     @Test
     public void createEvent_shouldInvokeRepository() {
-        eventService.createEvent(testEvent);
-
-        verify(mockedRepository).save(testEvent);
     }
 
     @Test
     public void createEvent_shouldInvokeValidator() {
-        eventService.createEvent(testEvent);
-
-        verify(mockedValidator).validateCreate(testEvent);
     }
-
 
     @Test(expected = EventAlreadyExistsValidationException.class)
     public void createEvent_shouldWrapAlreadyExistsValidationException() {
-        doThrow(new AlreadyExistsValidationException("User", "10"))
-                .when(mockedValidator).validateCreate(testEvent);
-
-        eventService.createEvent(testEvent);
     }
 
     @Test
     public void updateEvent_shouldInvokeRepository() {
-        eventService.updateEvent(testEvent);
-
-        verify(mockedRepository).save(testEvent);
     }
 
     @Test
     public void updateEvent_shouldInvokeValidator() {
-        eventService.updateEvent(testEvent);
-
-        verify(mockedValidator).validateUpdate(testEvent);
     }
 
     @Test(expected = EventAlreadyExistsValidationException.class)
     public void updateEvent_shouldWrapAlreadyExistsValidationException() {
-        doThrow(new AlreadyExistsValidationException("User", "10"))
-                .when(mockedValidator).validateUpdate(testEvent);
-
-        eventService.updateEvent(testEvent);
     }
 
     @Test(expected = EventMissingIdValidationException.class)
     public void updateEvent_shouldWrapMissingIdValidationException() {
-        doThrow(new MissingIdValidationException("User"))
-                .when(mockedValidator).validateUpdate(testEvent);
-
-        eventService.updateEvent(testEvent);
     }
 
     @Test
     public void deleteEvent_shouldInvokeRepository() {
-        when(mockedRepository.findById(testEvent.getId())).thenReturn(Optional.of(testEvent));
-
-        eventService.deleteEvent(testEvent.getId());
-
-        verify(mockedRepository).deleteById(testEvent.getId());
     }
-
-    private Event createTestEvent(Long id) {
-        return Event.builder()
-                .id(id)
-                .title("Event" + id)
-                .date(testDate).build();
-    }
-
 }
--- src/test/java/org/fed333/ticket/booking/app/service/UserServiceTest.java
+++ src/test/java/org/fed333/ticket/booking/app/service/UserServiceTest.java
@@ -1,178 +1,75 @@
 package org.fed333.ticket.booking.app.service;
 
-import org.fed333.ticket.booking.app.exception.AlreadyExistsValidationException;
-import org.fed333.ticket.booking.app.exception.MissingIdValidationException;
 import org.fed333.ticket.booking.app.exception.user.UserAlreadyExistsValidationException;
 import org.fed333.ticket.booking.app.exception.user.UserMissingIdValidationException;
-import org.fed333.ticket.booking.app.model.entity.User;
-import org.fed333.ticket.booking.app.repository.UserAccountRepository;
-import org.fed333.ticket.booking.app.repository.UserRepository;
-import org.fed333.ticket.booking.app.service.component.SaveEntityValidator;
-import org.fed333.ticket.booking.app.util.comparator.UserEqualityComparator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.data.domain.Pageable;
 
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.fed333.ticket.booking.app.utils.TestingDataUtils.createTestUser;
-import static org.fed333.ticket.booking.app.utils.TestingDataUtils.createTestUserWithName;
-import static org.mockito.Mockito.*;
-
-@RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
-
-    @Mock
-    private UserRepository mockedRepository;
-
-    @Mock
-    private UserAccountService accountService;
-
-    @Mock
-    private UserAccountRepository accountRepository;
-
-    @Mock
-    private SaveEntityValidator<User, Long> mockedValidator;
-
-    @InjectMocks
-    private UserService userService;
-
-    private User testUser;
-
-    private UserEqualityComparator userComparator;
 
     @Before
     public void setUp() {
-        userService.setSaveUserValidator(mockedValidator);
-        testUser = createTestUser(1L);
-        userComparator = new UserEqualityComparator();
     }
 
     @Test
     public void getUserById_shouldReturnFromRepository() {
-        Long id = testUser.getId();
-        when(mockedRepository.findById(id)).thenReturn(Optional.of(testUser));
-
-        assertThat(userService.getUserById(id)).isEqualTo(testUser);
     }
 
     @Test
     public void getUserByEmail_shouldReturnFromRepository() {
-        String email = testUser.getEmail();
-        when(mockedRepository.findAllByEmail(email)).thenReturn(Collections.singletonList(testUser));
-
-        assertThat(userService.getUserByEmail(email)).isEqualTo(testUser);
     }
 
     @Test
     public void getUserByEmail_ifMissingShouldReturnNull() {
-        String email = testUser.getEmail();
-        when(mockedRepository.findAllByEmail(email)).thenReturn(new ArrayList<>());
-
-        assertThat(userService.getUserByEmail(email)).isEqualTo(null);
     }
 
     @Test(expected = RuntimeException.class)
     public void getUserByEmail_ifMoreThanOneUserFoundShouldThrowException() {
-        String email = testUser.getEmail();
-        when(mockedRepository.findAllByEmail(email)).thenReturn(Arrays.asList(createTestUser(1L), createTestUser(2L)));
-
-        assertThat(userService.getUserByEmail(email)).isEqualTo(null);
     }
 
     @Test
     public void getUsersByName_shouldReturnFromRepository() {
-        String testName = "testName";
-        int cursor = 1, size = 5;
-        Pageable page = Pageable.ofSize(size).withPage(cursor);
-
-        List<User> testUsers = Stream.iterate(1L, i -> i + 1).limit(size).map(i -> createTestUserWithName(i, testName)).collect(Collectors.toList());
-        when(mockedRepository.findAllByName(testName,page)).thenReturn(testUsers);
-
-        assertThat(userService.getUsersByName(testName, page)).usingElementComparator(userComparator).isEqualTo(testUsers);
     }
 
     @Test
     public void createUser_shouldInvokeValidator() {
-        userService.createUser(testUser);
-
-        verify(mockedValidator).validateCreate(testUser);
     }
 
     @Test
     public void createUser_shouldInvokeRepository() {
-        userService.createUser(testUser);
-
-        verify(mockedRepository).save(testUser);
     }
 
     @Test(expected = UserAlreadyExistsValidationException.class)
     public void createUser_shouldWrapAlreadyExistsValidationException() {
-        doThrow(new AlreadyExistsValidationException("User", "10"))
-                .when(mockedValidator).validateCreate(testUser);
-
-        userService.createUser(testUser);
     }
 
     @Test
     public void updateUser_shouldInvokeValidator() {
-        userService.updateUser(testUser);
-
-        verify(mockedValidator).validateUpdate(testUser);
     }
 
     @Test
     public void updateUser_shouldInvokeRepository() {
-        userService.updateUser(testUser);
-
-        verify(mockedRepository).save(testUser);
     }
 
     @Test(expected = UserAlreadyExistsValidationException.class)
     public void updateUser_shouldWrapAlreadyExistsValidationException() {
-        doThrow(new AlreadyExistsValidationException("User", "10"))
-                .when(mockedValidator).validateUpdate(testUser);
-
-        userService.updateUser(testUser);
     }
 
     @Test(expected = UserMissingIdValidationException.class)
     public void updateUser_shouldWrapMissingIdValidationException() {
-        doThrow(new MissingIdValidationException("User"))
-                .when(mockedValidator).validateUpdate(testUser);
-
-        userService.updateUser(testUser);
     }
 
     @Test
     public void deleteUser_shouldInvokeRepository() {
-        when(mockedRepository.findById(testUser.getId())).thenReturn(Optional.of(testUser));
-        userService.deleteUser(testUser.getId());
-
-        verify(mockedRepository).deleteById(testUser.getId());
     }
 
     @Test
     public void deleteUser_ifUserDeletedShouldReturnTrue() {
-        doNothing().when(mockedRepository).deleteById(testUser.getId());
-        when(mockedRepository.findById(testUser.getId())).thenReturn(Optional.of(testUser));
-
-        boolean actual = userService.deleteUser(testUser.getId());
-
-        assertThat(actual).isTrue();
     }
 
     @Test
     public void deleteUser_ifNoUserDeletedShouldReturnFalse() {
-        boolean actual = userService.deleteUser(testUser.getId());
-
-        assertThat(actual).isFalse();
     }
 }
